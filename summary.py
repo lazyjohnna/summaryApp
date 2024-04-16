5adfdef5230869a09bc5dd5733ae97079f75c2bf
johnlab <160401136+lazyjohnna@users.noreply.github.com>
2024-04-16 09:59:13 +0900

    Add files via upload

---
+++ summary.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+import openai
+
+def askGpt(prompt):
+    messages_prompt = [{"role": "system", "content": prompt}]
+    response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages_prompt)
+    gptResponse = response["choices"][0]["message"]["content"]
+    return gptResponse
+
+##### 메인 함수 #####
+def main():
+    st.set_page_config(page_title="요약 프로그램")
+    # 사이드바
+    with st.sidebar:
+        # Open AI API 키 입력받기
+        open_apikey = st.text_input(label='OPENAI API 키', placeholder='Enter Your API Key', value='',type='password')    
+        # 입력받은 API 키 표시
+        if open_apikey:
+            openai.api_key  = open_apikey 
+        st.markdown('---')
+
+    st.header("📃요약 프로그램")
+    st.markdown('---')
+    
+    text = st.text_area("요약 할 글을 입력하세요")
+    if st.button("요약"):
+        prompt = f'''
+        **Instructions** :
+    - You are an expert assistant that summarizes text into **Korean language**.
+    - Your task is to summarize the **text** sentences in **Korean language**.
+    - Your summaries should include the following :
+        - Omit duplicate content, but increase the summary weight of duplicate content.
+        - Summarize by emphasizing concepts and arguments rather than case evidence.
+        - Summarize in 3 lines.
+        - Use the format of a bullet point.
+    -text : {text}
+    '''
+        st.info(askGpt(prompt))
+
+if __name__=="__main__":
+    main()
